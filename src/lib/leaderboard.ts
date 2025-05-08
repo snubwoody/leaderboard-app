5eb1795edaf494b305ba366711e92535ba2c2c89
waku <wakunguma13@gmail.com>
2025-05-08 16:54:53 -0600

    feat: add functions `createLeaderboard` and `getLeaderboards`

--- src/lib/leaderboard.ts
+++ src/lib/leaderboard.ts
@@ -1,4 +1,68 @@
+import type { PostgrestError } from "@supabase/supabase-js";
+import { supabase } from ".";
+import type { Result } from "./error";
 
-async function getLeaderboard(){
 
+export type LeaderboardMember = {
+    id: number, 
+    player: string,
+    player_alias: string | null,
+    leaderboard: number
+}
+
+export type Leaderboard = {
+    id: number,
+    name: string,
+    created_at:string,
+    members: LeaderboardMember[]
+}
+
+export async function getLeaderboards(){
+    const {data,error} = await supabase
+        .from('leaderboard')
+        .select('*, leaderboard_members(*)')
+
+    if (!data){
+        return {data,error}
+    }
+
+    const leaderboards = data.map(item => {
+        let leaderboard: Leaderboard = {
+            id: item.id,
+            name: item.name,
+            created_at: item.created_at,
+            members: item.leaderboard_members
+        };
+
+        return leaderboard
+    });
+
+    return {data:leaderboards,error}
+}
+
+
+export async function createLeaderboard(
+    name: string
+): Promise<Result<Leaderboard,PostgrestError>>
+{
+    const {data,error} = await supabase
+        .from('leaderboard')
+        .insert({name})
+        .select()
+        .single();
+
+    if (!data){
+        return {
+            data: null,
+            error
+        }
+    }
+    const leaderboard: Leaderboard = {
+        id: data.id,
+        name: data.name,
+        created_at: data.created_at,
+        members: []
+    }
+
+    return {data:leaderboard,error}
 }
--- src/tests/leaderboard.test.ts
+++ src/tests/leaderboard.test.ts
@@ -1,23 +1,24 @@
 import { test,expect } from "vitest";
 import { supabase } from "../lib";
+import { createLeaderboard, getLeaderboards } from "../lib/leaderboard";
 
 test('Create new leaderboard',async()=>{
-    const {data:{user},error:signInError} = await supabase.auth.signInAnonymously();
+    let {data:leaderboard,error} = await createLeaderboard('');
     
-    const {data,error} = await supabase
-        .from('leaderboard')
-        .insert({"name":"My leaderboard"})
-        .select()
-        .single()
-    
-    console.log(data)
-    
-    const {error:insertError} = await supabase.from('leaderboard_members').insert({
-        leaderboard:data?.id ?? 2, // FIXME
-        player:user?.id ??''
-    })
+    expect(error).toBe(null)
+    expect(leaderboard).not.toBe(null)
 
-    console.log(insertError)
-        
-    // console.log(error)
-})
+    let {data,error:selectError} = await supabase
+        .from('leaderboard')
+        .select('*')
+        .eq('id',leaderboard?.id ?? 0)
+        .single();
+
+    expect(selectError).toBe(null)
+    expect(data?.name).toBe(leaderboard?.name)
+});
+
+test('Get leaderboards',async()=>{
+    const {error} = await getLeaderboards();
+    expect(error).toBe(null)
+});
